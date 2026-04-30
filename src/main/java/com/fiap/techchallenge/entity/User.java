e33708cdd2b6b472183fcbb8305024ee1fea623c
Brayan Schroeder <brayan.schroeder@prontumais.com.br>
2026-04-30 15:41:11 -0300

    Ajustes na entidade de usuário

--- src/main/java/com/fiap/techchallenge/entity/User.java
+++ src/main/java/com/fiap/techchallenge/entity/User.java
@@ -7,22 +7,38 @@ import java.time.LocalDateTime;
 
 @Data
 @Entity
-@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "email"),
+                @UniqueConstraint(columnNames = "login")
+        }
+)
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false, unique = true)
     private String login;
+
+    @Column(nullable = false)
     private String password;
+
     private String address;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Role role;
 
+    @Column(nullable = false)
     private LocalDateTime lastUpdate;
 
     @PrePersist
